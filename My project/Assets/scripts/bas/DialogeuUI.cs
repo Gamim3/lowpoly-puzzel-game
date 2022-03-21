7f7768c027944020ad3a5ef4e42fec2f4cde0750
Gamim3 <basarendvanb@gmail.com>
2022-03-21 15:41:05 +0100

    dialogue update

--- My project/Assets/scripts/bas/DialogeuUI.cs
+++ My project/Assets/scripts/bas/DialogeuUI.cs
@@ -3,10 +3,10 @@ using TMPro;
 
 public class DialogeuUI : MonoBehaviour
 {
-    [SerializeField] private TMP_Text textlabel;
+    [SerializeField] private TMP_Text textLabel;
 
     private void Start()
     {
-        GetComponent<TypeWriterEffect>().Run(textToType: "this is a bit of text.\n Hello!", textlabel);
+        GetComponent<TypeWriterEffect>().Run(textToType: "this is a bit of text.\nHello!", textLabel);
     }
 }
--- My project/Assets/scripts/bas/TypeWriterEffect.cs
+++ My project/Assets/scripts/bas/TypeWriterEffect.cs
@@ -5,25 +5,34 @@ using TMPro;
 
 public class TypeWriterEffect : MonoBehaviour
 {
-    public void Run(string textToType, TMP_Text textlabel)
+    [SerializeField] private float typewriterSpeed = 50f;
+
+
+    public void Run(string textToType, TMP_Text textLabel)
     {
-        StartCoroutine(routine: TypeText(textToType, textlabel));
+        StartCoroutine(routine: TypeText(textToType, textLabel));
     }
 
-    private IEnumerator TypeText(string textToType, TMP_Text textlabel)
+    private IEnumerator TypeText(string textToType, TMP_Text textLabel)
     {
+        yield return new WaitForSeconds(2);
+
+
         float t = 0;
         int charIndex = 0;
 
         while (charIndex < textToType.Length)
         {
-            t += Time.deltaTime;
+            t += Time.deltaTime * typewriterSpeed;
             charIndex = Mathf.FloorToInt(t);
             charIndex = Mathf.Clamp(value: charIndex, min: 0, max: textToType.Length);
 
-            textlabel.text = textToType.Substring(startIndex: 0, length: charIndex);
+            textLabel.text = textToType.Substring(startIndex: 0, length: charIndex);
+
+
+            yield return null;
         }
 
-        textlabel.text = textToType;
+        textLabel.text = textToType;
     }
 }
