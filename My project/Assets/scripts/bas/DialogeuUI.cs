c6bb8886b251698f0e7dfed7dfe7400de80a4c1f
Gamim3 <basarendvanb@gmail.com>
2022-03-24 11:39:58 +0100

    Dialogue Update jesNO

--- My project/Assets/scripts/bas/DialogeuUI.cs
+++ My project/Assets/scripts/bas/DialogeuUI.cs
@@ -9,28 +9,42 @@ public class DialogeuUI : MonoBehaviour
     [SerializeField] private DialogueObject testDialogue;
 
     private TypeWriterEffect typewritereffect;
+    private ResponseHandler responseHandler;
 
     private void Start()
     {
         typewritereffect = GetComponent<TypeWriterEffect>();
         ShowDialogue(testDialogue);
+        responseHandler = GetComponent<ResponseHandler>();
     }
 
     public void ShowDialogue(DialogueObject dialogueObject)
     {
+        dialogueBox.SetActive(true);
         StartCoroutine(routine: StepThroughDialogue(dialogueObject));
         
     }
 
     private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
     {
-        foreach (string dialogue in dialogueObject.Dialogue)
+        for(int i = 0; i < dialogueObject.Dialogue.Length; i++)
         {
+            string dialogue = dialogueObject.Dialogue[i];
             yield return typewritereffect.Run(dialogue, textLabel);
+
+            if (i == dialogueObject.Dialogue.Length - 1 && dialogueObject.hasResponses) break;
             yield return new WaitUntil(() => Input.GetButtonDown("Fire1"));
+
         }
 
-        CloseDialogueBox();
+        if (dialogueObject.hasResponses)
+        {
+            responseHandler.ShowResponses(dialogueObject.Responses);
+        }
+        else
+        {
+            CloseDialogueBox();
+        }
     }
 
     private void CloseDialogueBox()
