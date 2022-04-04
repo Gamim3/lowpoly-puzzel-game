56c8d22dfa5cbd630eb6ba5b3c2a672416986cfe
YuhBappo <nathanbouwknegt@gmail.com>
2022-04-04 13:29:35 +0200

    LockPickTest scripts

---
+++ My project/Assets/scripts/Nathan/ThirdPinTest.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPinTest : MonoBehaviour
+{
+    public bool thirdPin;
+    public float rotationSpeed = 0.1f;
+    public Vector3 rot;
+    public float rotationFloat;
+    public float movementFloat;
+    public Vector3 lockPinRotation;
+    public int maxAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationFloat = Input.GetAxis("Horizontal");
+        rot.z = rotationFloat * rotationSpeed;
+        transform.Rotate(rot);
+        lockPinRotation = transform.eulerAngles;
+        float value = Random.Range(2.5f, 357.5f);
+        if (lockPinRotation.z > value - 2.5f && lockPinRotation.z < value + 2.5f)
+        {
+          thirdPin  = true;
+        }
+    }
+      
+}
+
